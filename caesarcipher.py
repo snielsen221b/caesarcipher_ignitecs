9625b4d9949cbac1c21fbf74956bfffeb73482a8
snielsen221b <sophianielsen221b@gmail.com>
2017-02-13 19:43:37 -0500

    look at that code, mmm

---
+++ caesarcipher.py
@@ -0,0 +1,12 @@
+# What is a caesar cipher?
+
+# ASCII/numbers for letters
+# >>> char(65)
+# 'A'
+# >>> ord('A')
+# 65
+
+max_key_size = 26
+
+def get_mode():
+    '''this function figures our if you'''
