db094c8e31c150dcc6e9b9ad727795819e16fae1
snielsen221b <sophianielsen221b@gmail.com>
2017-02-13 19:59:05 -0500

    look at that code, mmm

--- caesarcipher.py
+++ caesarcipher.py
@@ -8,5 +8,13 @@
 
 max_key_size = 26
 
+
 def get_mode():
-    '''this function figures our if you'''
+    '''this function figures our if your code
+    is encrypting and decrypting'''
+    # this is a while loop, it runs all the time if the condition is true
+    # the while tells you the type of loop, the True is your condition
+    # everything indented unded the while True: is in the loop
+    while True:
+        print("Do you wish to encrypt or dycrypt a message?")
+        mode = input().lower()
